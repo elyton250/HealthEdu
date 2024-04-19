1709c6bc271fbb072455911e7f046f3169896398
elyton250 <nelysemathieu@gmail.com>
2024-04-19 21:06:32 +0200

    mvp-page-made

---
+++ js/intro.js
@@ -0,0 +1,5 @@
+const header = document.querySelector("header");
+const menuToggler = document.querySelectorAll("#menu_toggle");
+menuToggler.forEach(toggler => {
+  toggler.addEventListener("click", () => header.classList.toggle("showMenu"));
+});
